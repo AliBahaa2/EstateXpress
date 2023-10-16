8b9455b050fde0c0dfaea4d37c4effd0fdce73f9
AliBahaa <101723971+AliBahaa2@users.noreply.github.com>
2023-10-16 19:58:16 +0300

    add

--- express/lib/main.dart
+++ express/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
           child: Text('hellooo Estate Xpress', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
