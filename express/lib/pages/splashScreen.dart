013ad33c8ed89291d77e60c42aa1911b8f980f64
AliBahaa <101723971+AliBahaa2@users.noreply.github.com>
2023-10-18 20:13:54 +0300

    add

--- express/lib/pages/splashScreen.dart
+++ express/lib/pages/splashScreen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'homeScreen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -15,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Timer(
-        const Duration(seconds: 3),
+        const Duration(seconds: 4),
         () => Navigator.of(context).pushReplacement(MaterialPageRoute(
             builder: (BuildContext context) => const HomeScreen())));
   }
@@ -23,22 +24,104 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 0, 140, 255),
-      body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
         children: [
-          Container(
-            height: 100,
-            width: 100,
-            child: Image.asset('lib/assets/images/splashLogo.png'),
+          const SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: Image(
+              image: AssetImage('lib/assets/images/background.jpg'),
+              fit: BoxFit.cover,
+            ),
           ),
-          const Text(
-            'Welcom to Express',
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+          Container(
+            color: const Color.fromARGB(255, 25, 62, 122).withOpacity(0.5),
+            child: Center(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 350,
+                  ),
+                  Animate(
+                    effects: [FadeEffect(), Effect()],
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 132,
+                          width: 132,
+                          child: Image.asset('lib/assets/images/splashLogo.png'),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 40),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'اهلا بك في',
+                                style: TextStyle(
+                                    fontFamily: 'Tajawal',
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                              Text(
+                                'Express',
+                                style: TextStyle(
+                                    fontFamily: 'Tajawal',
+                                    fontSize: 35,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 80),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              'يتيح لك هذا التطبيق بيع وشراء وتأجير كافة الممتلكات ' ,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 2,
+                              style: TextStyle(
+                                
+                                color: Colors.white,
+                                fontFamily: 'tajawal',
+                                fontSize: 16
+                              ),
+                            ),
+                            Text(
+                              ' .العقارية بكل سهولة' ,
+
+                              style: TextStyle(
+                                
+                                color: Colors.white,
+                                fontFamily: 'tajawal',
+                                fontSize: 16
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           ),
         ],
-      )),
+      ),
     );
   }
 }
