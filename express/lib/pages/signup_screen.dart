1d0362016acdf2a7abee96152d70a2b79f98c746
proc2001 <itproc879@gmail.com>
2023-10-24 15:01:14 -0700

    Create signup_screen.dart
    add

---
+++ express/lib/pages/signup_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+// import '../components/material_Button.dart';
+// import 'homeScreen.dart';
+// import 'package:flutter_sizer/flutter_sizer.dart';
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SignUpScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          //الصورة الي بالخلفية
+          const SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: Image(
+              image: AssetImage('lib/assets/images/background2.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+         
+        ],
+      ),
+    );
+  }
+}
+
