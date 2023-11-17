c0f1302f9aa2063aef9d538687a91b8a58c95e3e
AliBahaa <101723971+AliBahaa2@users.noreply.github.com>
2023-11-17 18:40:26 +0300

    add

--- express/lib/pages/navbarScreens/Home_NavBar.dart
+++ express/lib/pages/navbarScreens/Home_NavBar.dart
@@ -1,9 +1,9 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:express/components/MyColors.dart';
 import 'package:express/widgets/Drawer.dart';
 import 'package:express/widgets/customAppbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class NavBarHome extends StatefulWidget {
@@ -14,14 +14,13 @@ class NavBarHome extends StatefulWidget {
 }
 
 class _NavBarHomeState extends State<NavBarHome> {
-  int activeIndex = 0;
   final urlImages = [
     'lib/assets/images/CarouselSlider/Slider1.png',
     'lib/assets/images/CarouselSlider/Slider2.png',
     'lib/assets/images/CarouselSlider/Slider3.jpg',
     'lib/assets/images/CarouselSlider/Slider4.png',
   ];
-
+  int activeIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +29,10 @@ class _NavBarHomeState extends State<NavBarHome> {
       body: Column(
         children: [
           const CustomAppBar(),
+          SizedBox(
+            height: 1.h,
+          ),
+          //كلاس الصور المتحركة
           CarouselSlider.builder(
             options: CarouselOptions(
               height: 22.h,
@@ -46,13 +49,30 @@ class _NavBarHomeState extends State<NavBarHome> {
               return buildImage(urlImage, index);
             },
           ),
-          SizedBox(height: 1.h),
+
+          SizedBox(
+            height: 1.h,
+          ),
           buildIndicator(),
         ],
       ),
     );
   }
 
+  //الفانكشن الي تعرض انتقال الصور
+  Widget buildImage(String urlImage, int index) => Container(
+        margin: EdgeInsets.symmetric(horizontal: 1.w),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Image.asset(
+            height: double.infinity,
+            width: double.infinity,
+            urlImage,
+            fit: BoxFit.fill,
+          ),
+        ),
+      );
+  //الازرار الي يتحركن وي انتقال الصور
   Widget buildIndicator() => AnimatedSmoothIndicator(
         activeIndex: activeIndex,
         count: urlImages.length,
@@ -62,16 +82,3 @@ class _NavBarHomeState extends State<NavBarHome> {
         ),
       );
 }
-
-Widget buildImage(String urlImage, int index) => Container(
-      margin: EdgeInsets.symmetric(horizontal: 1.w),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: Image.asset(
-          height: double.infinity,
-          width: double.infinity,
-          urlImage,
-          fit: BoxFit.fill,
-        ),
-      ),
-    );
