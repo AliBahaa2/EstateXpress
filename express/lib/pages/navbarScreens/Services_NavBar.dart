567378a255331d9691dc426e6c8df899ebd55ea2
AliBahaa <101723971+AliBahaa2@users.noreply.github.com>
2023-12-03 21:15:59 +0300

    add

--- express/lib/pages/navbarScreens/Services_NavBar.dart
+++ express/lib/pages/navbarScreens/Services_NavBar.dart
@@ -1,4 +1,3 @@
-import 'package:express/components/customAppbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sizer/flutter_sizer.dart';
 
@@ -9,20 +8,129 @@ class NavBarServices extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Column(
-        children: [
-          const CustomAppBar(),
-          Row(
-            children: [
-              Container(
-                height: 10.h,
-                width: 7.w,
-                color: Colors.amber,
-              )
-            ],
-          )
-        ],
-      )),
+        child: Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20)),
+                color: Colors.white,
+              ),
+              height: 12.h,
+              child: Padding(
+                padding: EdgeInsets.only(left: 7.w, right: 7.w, top: 3.h),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Text(
+                        'الخدمات',
+                        style: TextStyle(
+                          fontFamily: 'cairo',
+                          fontSize: 25.dp,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 5.h,
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20.h,
+                          width: 30.w,
+                          child: const Placeholder(),
+                        ),
+                        SizedBox(
+                          width: 5.w,
+                        ),
+                        SizedBox(
+                          height: 20.h,
+                          width: 30.w,
+                          child: const Placeholder(),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20.h,
+                          width: 30.w,
+                          child: const Placeholder(),
+                        ),
+                        SizedBox(
+                          width: 5.w,
+                        ),
+                        SizedBox(
+                          height: 20.h,
+                          width: 30.w,
+                          child: const Placeholder(),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20.h,
+                          width: 30.w,
+                          child: const Placeholder(),
+                        ),
+                        SizedBox(
+                          width: 5.w,
+                        ),
+                        SizedBox(
+                          height: 20.h,
+                          width: 30.w,
+                          child: const Placeholder(),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 5.h,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 20.h,
+                          width: 30.w,
+                          child: const Placeholder(),
+                        ),
+                        SizedBox(
+                          width: 5.w,
+                        ),
+                        SizedBox(
+                          height: 20.h,
+                          width: 30.w,
+                          child: const Placeholder(),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
